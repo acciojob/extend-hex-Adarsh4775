4c861bbd7102d8ec1d4d3dfb14789cc84f0008a9
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-11 21:41:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-const extendHex = (shortHex) => {
-  // write your code here
-};
 
+  // write your code here
+	const extendHex = (shortHex) => {
+  // Check and add # prefix if needed
+  if (!shortHex.startsWith("#")) {
+    shortHex = "#" + shortHex;
+  }
+
+  // Initialize the full hex code as an empty string
+  let fullHex = "";
+
+  // Iterate over each character in shortHex
+  for (let i = 1; i < shortHex.length; i++) {
+    // Duplicate each character and append to fullHex
+    fullHex += shortHex[i] + shortHex[i];
+  }
+
+  // Return the extended hex code
+  return fullHex;
+}; 
+
+
+ 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
